b6260d9a6ae5c40c0cfa009e3e838f7e633f1783
Alden <Alden@gmail.com>
2022-11-29 16:58:44 -0600

    class notes 11/29

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
 
+    """The home page for Learning Log."""
     return render(request, 'MainApp/index.html')
+
+def topics(request):
+
+    topics = Topic.objects.order_by('-date_added')
+
+    context = {'t':topics}
+
+    return render(request, 'MainApp/topics.html', context)
+
+def topic(request, topic_id):
+
+    t = Topic.objects.get(id = topic_id)
+    entries = Entry.objects.filter(topic = t)
+
+    context = {'topic':t, 'entries':entries}
+
+    return render(request, 'MainApp/topic.html', context)
 
     
